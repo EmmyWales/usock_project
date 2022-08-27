8a5e42d6de699480ee71a5f448417d3fe9e8eb41
Emmy Wales <emmanueladewale962@gmail.com>
2022-08-27 16:37:34 +0100

    first commit

--- lib/screens/pages/profile.dart
+++ lib/screens/pages/profile.dart
@@ -13,132 +13,189 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 100,
-                  width: 1000,
-                  color: Colors.white,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.yellow,
-                            borderRadius: BorderRadius.circular(18),
-                            image: const DecorationImage(
-                                image: AssetImage("assets/man.jpeg"),
-                                fit: BoxFit.fill)),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "William John Malik",
-                              style: TextStyle(
-                                  color: AppColor.primary,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w800),
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              "Aggressive Investor",
-                              style: TextStyle(
-                                  color: AppColor.txtblue,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.normal),
-                            ),
-                          ],
+        body: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 1000,
+                    color: Colors.white,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              color: Colors.yellow,
+                              borderRadius: BorderRadius.circular(18),
+                              image: const DecorationImage(
+                                  image: AssetImage("assets/man.jpeg"),
+                                  fit: BoxFit.fill)),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                ListTile(
-                    title: const Text('Personal Data', style: TextStyle(fontWeight: FontWeight.w600, color: Color(0XFF395878)),),
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (_) => const Status()));
-                    },
-                    leading: Container(
-                      color: AppColor.txtblue,
-                      child: Icon(
-                        Icons.person,
-                        color: AppColor.primary,
-                      ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "William John Malik",
+                                style: TextStyle(
+                                    color: AppColor.primary,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w800),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Aggressive Investor",
+                                style: TextStyle(
+                                    color: AppColor.txtblue,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    trailing: const Icon(
-                      Icons.arrow_forward_ios,
-                    )),
-                ListTile(
-                    title: const Text('Settings', style: TextStyle(fontWeight: FontWeight.w600, color: Color(0XFF395878),)),
-                    leading: Container(
-                        color: AppColor.txtblue,
-                        child: Icon(
-                          Icons.settings,
-                          color: AppColor.primary,
-                          
-                        )),
-                    trailing: const Icon(
-                      Icons.arrow_forward_ios,
-                    )),
-                ListTile(
-                    title: const Text('E-Statement',  style: TextStyle(fontWeight: FontWeight.w600, color: Color(0XFF395878))),
-                    leading: Container(
-                        color: AppColor.txtblue,
-                        child: Icon(
-                          Icons.file_open,
-                          color: AppColor.primary,
-                        )),
-                    trailing: const Icon(
-                      Icons.arrow_forward_ios,
-                    )),
-                ListTile(
-                    title: const Text('Referral Code',  style: TextStyle(fontWeight: FontWeight.w600, color: Color(0XFF395878))),
-                    leading: Container(
-                        color: AppColor.txtblue,
-                        child: Icon(
-                          Icons.hd,
-                          color: AppColor.primary,
-                        )),
-                    trailing: const Icon(
-                      Icons.arrow_forward_ios,
-                    )),
-                ListTile(
-                    title: const Text("FAQS",  style: TextStyle(fontWeight: FontWeight.w600, color: Color(0XFF395878))),
-                    leading: Container(
+                  ),
+                  ListTile(
+                      title: const Text(
+                        'Personal Data',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Color(0XFF395878)),
+                      ),
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (_) => const Status()));
+                      },
+                      leading: Container(
                         color: AppColor.txtblue,
                         child: Icon(
                           Icons.person,
                           color: AppColor.primary,
-                        )),
-                    trailing: const Icon(
-                      Icons.arrow_forward_ios,
-                    )),
-                Center(
-                  // ignore: sized_box_for_whitespace
-                  child: Container(
-                    height: 30,
-                    width: 600,
-                    child: Row(
-                      children: const [
-                        Text(
-                          "Feel free to ask. we are ready to help",
-                        )
-                      ],
+                        ),
+                      ),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                      )),
+                  ListTile(
+                      title: const Text('Settings',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Color(0XFF395878),
+                          )),
+                      leading: Container(
+                          color: AppColor.txtblue,
+                          child: Icon(
+                            Icons.settings,
+                            color: AppColor.primary,
+                          )),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                      )),
+                  ListTile(
+                      title: const Text('E-Statement',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Color(0XFF395878))),
+                      leading: Container(
+                          color: AppColor.txtblue,
+                          child: Icon(
+                            Icons.description,
+                            color: AppColor.primary,
+                          )),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                      )),
+                  ListTile(
+                      title: const Text('Referral Code',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Color(0XFF395878))),
+                      leading: Container(
+                          color: AppColor.txtblue,
+                          child: Icon(
+                            Icons.favorite,
+                            color: AppColor.primary,
+                          )),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                      )),
+        
+                      const SizedBox(height: 25,),
+                  ListTile(
+                      title: const Text("FAQS",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Color(0XFF395878))),
+                      leading: Container(
+                          color: AppColor.txtblue,
+                          child: Icon(
+                            Icons.pending,
+                            color: AppColor.primary,
+                          )),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                      )),
+                      ListTile(
+                      title: const Text("Our Handbook",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Color(0XFF395878))),
+                      leading: Container(
+                          color: AppColor.txtblue,
+                          child: Icon(
+                            Icons.drive_file_rename_outline,
+                            color: AppColor.primary,
+                          )),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                      )),
+                       ListTile(
+                      title: const Text("Community",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Color(0XFF395878))),
+                      leading: Container(
+                          color: AppColor.txtblue,
+                          child: Icon(
+                            Icons.groups,
+                            color: AppColor.primary,
+                          )),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                      )),
+                      const SizedBox(height: 20,),
+                  Center(
+                    // ignore: sized_box_for_whitespace
+                    child: Container(
+                      height: 60,
+                      width: 600,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 220, 234, 241),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.headset_mic, color: Color(0XFF395878),),
+                          Text(
+                            " Feel Free to Ask. We are Ready to Help",
+                            style: TextStyle(color: Color(0XFF395878), fontSize: 16, fontWeight: FontWeight.w600),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                )
-              ],
-            )));
+                  )
+                ],
+              )),
+        ));
   }
 }
